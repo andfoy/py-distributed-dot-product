2f3c7dd7e462ffa3303bac83884211758064919a
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-04-28 11:35:02 -0500

    Add TN benchmark

--- benchmark.py
+++ benchmark.py
@@ -32,6 +32,8 @@ parser.add_argument('--mode', type=str, default='nt',
                     help='Benchmark mode')
 parser.add_argument('--offset', type=int, default=1000,
                     help='Offset used to balance time/memory')
+parser.add_argument('--scale', type=int, default=1,
+                    help='Scale factor used for reduction')
 parser.add_argument('--file', type=str, default="nt_benchmark.json",
                     help='JSON file used to append results')
 args = parser.parse_args()
@@ -68,7 +70,7 @@ def measure(function, *args, **kwargs):
 def nt_benchmark():
     # Benchmark NT multiplication (local node)
     if is_main_process():
-        xlarge = torch.rand(1, 75000, 768, device=device)
+        xlarge = torch.rand(1, 75000 // args.scale, 768, device=device)
         y = xlarge.transpose(-1, -2)
         input_memory = torch.cuda.memory_allocated()
         print(f'Memory allocated by xlarge/y: '
@@ -84,7 +86,7 @@ def nt_benchmark():
         torch.cuda.empty_cache()
 
     # Benchmark TN multiplication (distributed)
-    xsmall = torch.rand(1, 75000 // 3, 768, device=device)
+    xsmall = torch.rand(1, 75000 // (3 * args.scale), 768, device=device)
     dist_input_size = torch.cuda.memory_allocated()
     print(f'Memory allocated by xsmall: '
         f'{humanize.naturalsize(dist_input_size)}')
@@ -114,60 +116,12 @@ def nt_benchmark():
                 avg_op_time, avg_peak_memory, avg_output_memory)
 
 
-def tn_benchmark():
-    # Benchmark NT multiplication (local node)
-    if is_main_process():
-        xlarge = torch.rand(1, 75000, 768, device=device)
-        y = xlarge.transpose(-1, -2)
-        input_memory = torch.cuda.memory_allocated()
-        print(f'Memory allocated by xlarge/y: '
-            f'{humanize.naturalsize(input_memory)}')
-        result, op_time, peak_memory = measure(torch.matmul, xlarge, y)
-        del xlarge
-        del y
-        torch.cuda.empty_cache()
-        output_memory = torch.cuda.memory_allocated()
-        print(f'matmul_nt - Output memory consumption: '
-            f'{humanize.naturalsize(output_memory)}')
-        del result
-        torch.cuda.empty_cache()
-
-    # Benchmark TN multiplication (distributed)
-    xsmall = torch.rand(1, 75000 // 3, 768, device=device)
-    dist_input_size = torch.cuda.memory_allocated()
-    print(f'Memory allocated by xsmall: '
-        f'{humanize.naturalsize(dist_input_size)}')
-    synchronize()
-    result, dop_time, dpeak_memory = measure(
-        distributed_matmul_nt, xsmall, xsmall, offset=args.offset)
-    del xsmall
-    torch.cuda.empty_cache()
-    doutput_memory = torch.cuda.memory_allocated()
-    print(f'distributed_matmul_nt - Output memory consumption: '
-        f'{humanize.naturalsize(doutput_memory)}')
-    del result
-    torch.cuda.empty_cache()
-
-    all_input_size = comm.gather(dist_input_size, root=0)
-    all_op_time = comm.gather(dop_time, root=0)
-    all_peak_memory = comm.gather(dpeak_memory, root=0)
-    all_output_memory = comm.gather(doutput_memory, root=0)
-
-    if is_main_process():
-        avg_input_size = sum(all_input_size) / len(all_input_size)
-        avg_op_time = sum(all_op_time) / len(all_op_time)
-        avg_peak_memory = sum(all_peak_memory) / len(all_peak_memory)
-        avg_output_memory = sum(all_output_memory) / len(all_output_memory)
-
-        return (input_memory, op_time, peak_memory, avg_input_size,
-                avg_op_time, avg_peak_memory, avg_output_memory)
-
-
 def all_benchmark():
     # Benchmark all multiplication (local node)
     if is_main_process():
-        xlarge = torch.rand(1, 75000, 75000, device=device)
-        y = torch.rand(1, 75000, 768, device=device)
+        xlarge = torch.rand(1, 75000 // args.scale, 75000 // args.scale,
+                            device=device)
+        y = torch.rand(1, 75000 // args.scale, 768, device=device)
         input_memory = torch.cuda.memory_allocated()
         print(f'Memory allocated by xlarge/y: '
             f'{humanize.naturalsize(input_memory)}')
@@ -182,8 +136,9 @@ def all_benchmark():
         torch.cuda.empty_cache()
 
     # Benchmark all multiplication (distributed)
-    xsmall = torch.rand(1, 75000 // 3, 75000, device=device)
-    ysmall = torch.rand(1, 75000 // 3, 768, device=device)
+    xsmall = torch.rand(1, 75000 // (3 * args.scale), 75000 // args.scale,
+                        device=device)
+    ysmall = torch.rand(1, 75000 // (3 * args.scale), 768, device=device)
     dist_input_size = torch.cuda.memory_allocated()
     print(f'Memory allocated by xsmall/ysmall: '
           f'{humanize.naturalsize(dist_input_size)}')
@@ -216,10 +171,66 @@ def all_benchmark():
                 avg_output_memory)
 
 
+def tn_benchmark():
+    # Benchmark tn multiplication (local node)
+    if is_main_process():
+        xlarge = torch.rand(1, 75000 // args.scale, 75000 // args.scale,
+                            device=device).transpose(-1, -2)
+        y = torch.rand(1, 75000 // args.scale, 768, device=device)
+        input_memory = torch.cuda.memory_allocated()
+        print(f'Memory allocated by xlarge/y: '
+            f'{humanize.naturalsize(input_memory)}')
+        result, op_time, peak_memory = measure(torch.matmul, xlarge, y)
+        del xlarge
+        del y
+        torch.cuda.empty_cache()
+        output_memory = torch.cuda.memory_allocated()
+        print(f'matmul_nt - Output memory consumption: '
+            f'{humanize.naturalsize(output_memory)}')
+        del result
+        torch.cuda.empty_cache()
+
+    # Benchmark tn multiplication (distributed)
+    xsmall = torch.rand(1, 75000 // (3 * args.scale), 75000 // args.scale,
+                        device=device)
+    ysmall = torch.rand(1, 75000 // (3 * args.scale), 768, device=device)
+    dist_input_size = torch.cuda.memory_allocated()
+    print(f'Memory allocated by xsmall/ysmall: '
+          f'{humanize.naturalsize(dist_input_size)}')
+    synchronize()
+    result, dop_time, dpeak_memory = measure(
+        distributed_matmul_tn, xsmall, ysmall, offset=args.offset)
+    del xsmall
+    del ysmall
+
+    torch.cuda.empty_cache()
+    doutput_memory = torch.cuda.memory_allocated()
+    print(f'distributed_matmul_all - Output memory consumption: '
+        f'{humanize.naturalsize(doutput_memory)}')
+    del result
+    torch.cuda.empty_cache()
+
+    all_input_size = comm.gather(dist_input_size, root=0)
+    all_op_time = comm.gather(dop_time, root=0)
+    all_peak_memory = comm.gather(dpeak_memory, root=0)
+    all_output_memory = comm.gather(doutput_memory, root=0)
+
+    if is_main_process():
+        avg_input_size = sum(all_input_size) / len(all_input_size)
+        avg_op_time = sum(all_op_time) / len(all_op_time)
+        avg_peak_memory = sum(all_peak_memory) / len(all_peak_memory)
+        avg_output_memory = sum(all_output_memory) / len(all_output_memory)
+
+        return (input_memory, output_memory, op_time, peak_memory,
+                avg_input_size, avg_op_time, avg_peak_memory,
+                avg_output_memory)
+
+
 def main():
     test_funcs = {
         'nt': nt_benchmark,
-        'all': all_benchmark
+        'all': all_benchmark,
+        'tn': tn_benchmark
     }
     output = test_funcs[args.mode]()
     if is_main_process():
